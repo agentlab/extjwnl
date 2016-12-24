429f5875186282a5c93569b866ec447c6a370c7b
DmitryZagr <zagd@1c.ru>
2016-12-25 00:21:46 +0300

    Add multi dictionaries support

--- plugins/net.sf.extjwnl.component/src/net/sf/extjwnl/component/ExtjwnlComponent.java
+++ plugins/net.sf.extjwnl.component/src/net/sf/extjwnl/component/ExtjwnlComponent.java
@@ -3,12 +3,15 @@
  */
 package net.sf.extjwnl.component;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.eclipse.core.runtime.Platform;
+import org.osgi.framework.Bundle;
 import org.osgi.service.component.ComponentContext;
 import org.osgi.service.component.annotations.Activate;
 
@@ -23,34 +26,80 @@ import net.sf.extjwnl.service.IExtjwnlService;
 public class ExtjwnlComponent
     implements IExtjwnlService {
 
-    private List<Dictionary> dictionary = new LinkedList<>();
+    private List<Dictionary> dictionaries = new LinkedList<>();
+    private final String pluginInstallDir = getPluginDir("net.sf.extjwnl.data.wn31");
+    private final String pathPattern = "_resource_properties.xml";
 
     @Override
     public List<Dictionary> getDictionary() {
-        return dictionary;
+        return dictionaries;
     }
 
     @Activate
-    public void activate(ComponentContext context) throws IOException {
+    public void activate(ComponentContext context) {
+        buildDictionaries();
+    }
 
+    private synchronized void buildDictionaries() {
         try
         {
-            URL url = new URL("platform:/plugin/net.sf.extjwnl.data.wn31/extjwnl_resource_properties.xml");
-//            URL url = new URL("platform:/plugin/net.sf.extjwnl.data.wn31");
-//            final File folder = new File(url.getContent());
-//            FileUtils;
+            final File folder = new File(pluginInstallDir);
 
-            InputStream inputStream = url.openConnection().getInputStream();
-            dictionary.add(Dictionary.getInstance(inputStream));
+            for (final File fileEntry : folder.listFiles())
+            {
+                if (fileEntry.isFile() && fileEntry.toString().contains(pathPattern))
+                {
+                    InputStream inputStream = fileEntry.toURI().toURL().openStream();
+                    dictionaries.add(Dictionary.getInstance(inputStream));
+                }
+            }
+
         }
-        catch (JWNLException e)
+        catch (JWNLException | IOException e)
         {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
 
-        System.err.println("Start !!!!"); //$NON-NLS-1$
+    }
 
+    public static String getPluginDir(String pluginId) {
+        /* get bundle with the specified id */
+        // pluginId = pluginId.
+        Bundle bundle = Platform.getBundle(pluginId);
+        if (bundle == null)
+        {
+            throw new RuntimeException("Could not resolve plugin: " + pluginId + "\r\n"
+                + "Probably the plugin has not been correctly installed.\r\n"
+                + "Running eclipse from shell with -clean option may rectify installation.");
+        }
+
+        /* resolve Bundle::getEntry to local URL */
+        URL pluginURL = null;
+        try
+        {
+            pluginURL = Platform.resolve(bundle.getEntry("/"));
+        }
+        catch (IOException e)
+        {
+            throw new RuntimeException("Could not get installation directory of the plugin: " + pluginId);
+        }
+        String pluginInstallDir = pluginURL.getPath().trim();
+        if (pluginInstallDir.length() == 0)
+        {
+            throw new RuntimeException("Could not get installation directory of the plugin: " + pluginId);
+        }
+
+        /* since path returned by URL::getPath starts with a forward slash, that
+         * is not suitable to run commandlines on Windows-OS, but for Unix-based
+         * OSes it is needed. So strip one character for windows. There seems
+         * to be no other clean way of doing this. */
+        /* if (Platform.getOS().compareTo(Platform.OS_MACOSX) == 0)
+        {
+            pluginInstallDir = pluginInstallDir.substring(1);
+        }*/
+
+        return pluginInstallDir;
     }
 
 }
